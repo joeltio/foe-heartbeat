10ba0bfa761969a369f669fb282b442b0f3cfab0
joeltio <joeltiojinhon@gmail.com>
2017-08-03 00:09:36 +0800

    Fixed CalibrateActivity to manage fragments properly Added other methods to CalibrateActivity as well

--- Pulse/app/src/main/java/joeltio/pulse/CalibrateActivity.java
+++ Pulse/app/src/main/java/joeltio/pulse/CalibrateActivity.java
@@ -1,9 +1,15 @@
 package joeltio.pulse;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import joeltio.pulse.Fragments.CalibrateFragment;
+import joeltio.pulse.Fragments.CalibrateIntroFragment;
+
 public class CalibrateActivity extends AppCompatActivity {
+
+    public static String EXTRA_MEAN_VALUE = "joeltio.pulse.CalibrateActivity.EXTRA_MEAN_VALUE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,5 +17,31 @@ public class CalibrateActivity extends AppCompatActivity {
         setContentView(R.layout.activity_calibrate);
 
         getSupportActionBar().hide();
+
+        CalibrateIntroFragment calibrateIntroFragment = new CalibrateIntroFragment();
+        getFragmentManager().beginTransaction().add(
+                R.id.calibrate_fragment,
+                calibrateIntroFragment,
+                calibrateIntroFragment.getTag()
+        ).commit();
+    }
+
+    public void startCalibration() {
+        CalibrateFragment calibrateFragment = new CalibrateFragment();
+        getFragmentManager().beginTransaction().replace(
+                R.id.calibrate_fragment,
+                calibrateFragment,
+                calibrateFragment.getTag()
+        ).commit();
+    }
+
+    public void stopCalibration(Double meanValue) {
+        startNextActivity(meanValue);
+    }
+
+    private void startNextActivity(Double meanValue) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra(EXTRA_MEAN_VALUE, meanValue);
+        startActivity(intent);
     }
 }
