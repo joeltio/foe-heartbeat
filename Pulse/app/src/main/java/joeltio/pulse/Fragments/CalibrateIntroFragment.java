7d181618072a3b3b6f4f2bb4c848c774f92b4d0a
joeltio <joeltiojinhon@gmail.com>
2017-08-02 23:02:21 +0800

    Changed to app.Fragment instead of support v4 Fragment

--- Pulse/app/src/main/java/joeltio/pulse/Fragments/CalibrateIntroFragment.java
+++ Pulse/app/src/main/java/joeltio/pulse/Fragments/CalibrateIntroFragment.java
@@ -1,8 +1,8 @@
 package joeltio.pulse.Fragments;
 
+import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,9 +10,7 @@ import android.view.ViewGroup;
 import joeltio.pulse.R;
 
 public class CalibrateIntroFragment extends Fragment {
-    public CalibrateIntroFragment() {
-        // Required empty public constructor
-    }
+    public CalibrateIntroFragment() {}
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
